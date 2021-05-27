b9c95306d78ac245f7cb8e20bffe50bc84b90f79
dragonnomada <dragonnomada123@gmail.com>
2021-05-27 11:50:07 -0500

    Se agregó el envio de formularios

--- cfe-apartarrayos/app/app.js
+++ cfe-apartarrayos/app/app.js
@@ -1,7 +1,11 @@
+const SERVER_URL = "http://localhost:8080";
+
+console.log("ACTIALIZA");
+
 async function getForms() {
     // TODO: Obtener los formularios del servidor
 
-    const response = await fetch("http://localhost:8080/api/apartarrayos/form");
+    const response = await fetch(`${SERVER_URL}/api/apartarrayos/form`);
 
     if (!response.ok) {
         // El servidor falló
@@ -34,7 +38,7 @@ async function getFormsAndDisplay() {
 async function getFormSchema(form) {
     // TODO: Obtener el esquema del servidor
 
-    const response = await fetch(`http://localhost:8080/api/apartarrayos/form/${form.id_form}/schema`);
+    const response = await fetch(`${SERVER_URL}/api/apartarrayos/form/${form.id_form}/schema`);
 
     if (!response.ok) {
         // El servidor falló
@@ -54,13 +58,58 @@ async function getFormSchema(form) {
     return result.schema;
 }
 
+function collectFormData(formNode) {
+    // TODO: Extraer los datos del formulario
+
+    const formData = new FormData(formNode);
+    return Object.fromEntries(formData);
+}
+
+async function submitForm(formData) {
+    // TODO: Envia el formulario al del servidor
+
+    const response = await fetch(`${SERVER_URL}/api/apartarrayos/form/${formData.id_form}/submit`, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+    });
+
+    if (!response.ok) {
+        // El servidor falló
+        const error = await response.text();
+        throw new Error("Falló el servidor?", error);
+    }
+
+    // { success, error?, originalError?, schema? }
+    const result = await response.json();
+
+    console.log("result", result);
+
+    if (!result.success) {
+        // TODO: Notificar al usuario que surgió un error
+        console.warn(result.error, result.originalError);
+        return false;
+    }
+
+    alert("Formulario Eviado");
+
+    return true;
+}
+
 async function buildForm(formSchema) {
     const formNode = document.createElement("form");
+
+    formNode.action = "#";
 
     formNode.innerHTML = `
         <h1>${formSchema.title} <small>${formSchema.version}</small></h1>
 
         <div>
+            <input type="hidden" id="id_form" name="id_form" value="${formSchema.id_form}" >
+            <input type="hidden" id="name" name="name" value="${formSchema.name}" >
+
             ${
                 formSchema.fields.map(field => {
                     return `
@@ -77,7 +126,57 @@ async function buildForm(formSchema) {
                 }).join("\n")
             }
         </div>
+
+        <div>   
+            <input type="button" class="form-btn-reset" value="Reiniciar">
+            <input type="button" class="form-btn-submit" value="Enviar">
+        </div>
     `;
+
+    // const formNode = formNode.querySelector("form");
+    const btnReset = formNode.querySelector(".form-btn-reset");
+    const btnSubmit = formNode.querySelector(".form-btn-submit");
+
+    // console.log(formContainerNode.outerHTML);
+
+    async function submit() {
+        // TODO: RECOLECTAR LOS DATOS DEL FORMULARIO
+        const formData = collectFormData(formNode);
+
+        console.log(JSON.stringify(formData));
+
+        // TODO: Enviar los datos del formulario al server
+
+        btnReset.disabled = true;
+        btnSubmit.disabled = true;
+
+        await submitForm(formData).catch(error => {
+            console.warn(`Error al enviar el formulario`, error);
+        });
+
+        btnReset.disabled = false;
+        btnSubmit.disabled = false;
+    };
+
+    formNode.addEventListener("submit", event => {
+        console.log("SUBMIT", formNode);
+
+        event.preventDefault();
+        event.stopPropagation();
+    });
+
+    btnReset.addEventListener("click", async event => {
+        console.log("RESETEANDO...", formSchema);
+        const newFormContainerNode = await buildForm(formSchema);
+        console.log(newFormContainerNode, formNode);
+        formNode.parentElement.replaceChild(newFormContainerNode, formNode);
+    });
+
+    btnSubmit.addEventListener("click", event => {
+        console.log("hola", formNode);
+        event.preventDefault();
+        submit();
+    });
 
     return formNode;
 }
--- cfe-apartarrayos/server/api/form.js
+++ cfe-apartarrayos/server/api/form.js
@@ -34,47 +34,39 @@ router.get("/api/apartarrayos/form", (request, response) => {
     });
 });
 
-// Devuelve el esquema (campos) de un formulario
-// Entrada: { formName }
-// Salida: { id_form, name, title, version, fields: [ { idform_fields, id_form, name, type, default_value, label } ] }
-router.get("/api/apartarrayos/form/:name/schema", (request, response) => {
-    const { name } = request.params;
-
-    const sql = `SELECT * FROM apartarrayos.form where id_form=?;`;
-
-    const values = [name];
-
-    mysql.connection().query(sql, values, (error, results, fields) => {
-        if (error) {
-            response.send({
-                success: false,
-                error: `Error al consultar el formulario`,
-                originalError: `${error}`
-            });
-            return;
-        }
-
-        if (results.length === 0) {
-            response.send({
-                success: false,
-                error: `No se encuentra el formulario llamado [${name}]`
-            });
-            return;
-        }
-
-        const form = results[0];
-
-        // TODO: Agregar los campos del formulario
-
+async function getFormFields(id_form) {
+    return await new Promise((resolve, reject) => {
         const sql = "SELECT * FROM apartarrayos.form_fields where id_form=?;"
 
-        const values = [name];
+        const values = [id_form];
 
         mysql.connection().query(sql, values, (error, results, fields) => {
             if (error) {
+                reject(error);
+                return;
+            }
+
+            if (results.length === 0) {
+                reject(`Error El formulario [${id_form}] no tiene campos`);
+                return;
+            }
+
+            resolve(results);
+        });
+    });
+}
+
+async function getFormSchema(id_form) {
+    return await new Promise((resolve, reject) => {
+        const sql = `SELECT * FROM apartarrayos.form where id_form=?;`;
+
+        const values = [id_form];
+
+        mysql.connection().query(sql, values, async (error, results, fields) => {
+            if (error) {
                 response.send({
                     success: false,
-                    error: `Error al consultar los campos del formulario [${name}]`,
+                    error: `Error al consultar el formulario`,
                     originalError: `${error}`
                 });
                 return;
@@ -83,25 +75,150 @@ router.get("/api/apartarrayos/form/:name/schema", (request, response) => {
             if (results.length === 0) {
                 response.send({
                     success: false,
-                    error: `Error El formulario [${name}] no tiene campos`
+                    error: `No se encuentra el formulario llamado [${id_form}]`
                 });
                 return;
             }
 
-            response.send({
-                success: true,
-                schema: {
-                    ...form,
-                    fields: results
-                }
-            });
-        });
+            const form = results[0];
 
-        // console.log(error);
-        // console.log(results);
-        // console.log(fields);
-        // response.send("ok");
+            // TODO: Agregar los campos del formulario
+
+            try {
+                const fields = await getFormFields(id_form);
+
+                resolve({
+                    ...form,
+                    fields
+                });
+
+            } catch (error) {
+                reject(error);
+            }
+        });
     });
+}
+
+// Devuelve el esquema (campos) de un formulario
+// Entrada: { formName }
+// Salida: { id_form, name, title, version, fields: [ { idform_fields, id_form, name, type, default_value, label } ] }
+router.get("/api/apartarrayos/form/:name/schema", async (request, response) => {
+    const { name } = request.params;
+
+    try {
+        const formSchema = await getFormSchema(name);
+
+        response.send({
+            success: true,
+            schema: formSchema
+        });
+    } catch (error) {
+        response.send({
+            success: false,
+            error: `Error al consultar los campos del formulario [${name}]`,
+            originalError: `${error}`
+        });
+    }
 });
+
+async function createRecord(id_form) {
+    return await new Promise((resolve, reject) => {
+        const sql = "INSERT INTO apartarrayos.form_records SET ?;"
+
+        const values = {
+            id_form,
+            created_at: new Date()
+        };
+
+        mysql.connection().query(sql, values, (error, result, fields) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+
+            resolve(result.insertId);
+        });
+    });
+}
+
+async function insertFormFieldValue(id_form, id_record, id_form_field, value) {
+    return await new Promise((resolve, reject) => {
+        const sql = "INSERT INTO apartarrayos.form_registers SET ?;"
+
+        const values = {
+            id_form,
+            id_record,
+            id_form_field,
+            value
+        };
+
+        mysql.connection().query(sql, values, (error, result, fields) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+
+            resolve(result.insertId);
+        });
+    });
+}
+
+async function insertFormRecord(formSchema, fields) {
+    // TODO: Insertar el registro en el formulario (bd)
+
+    // 1. CREAR UN REGISTRO (RECORD) (SESIÓN / SUBMIT)
+
+    const recordId = await createRecord(formSchema.id_form);
+
+    console.log({ recordId });
+
+    // 2. AGREGAR LOS VALORES DEL REGISTRO (REGISTER) (A LA SESIÓN)
+
+    const ids = [];
+
+    for (let [fieldName, fieldValue] of Object.entries(fields)) {
+        const [field] = formSchema.fields.filter(({ name }) => name === fieldName);
+
+        console.log({ field });
+
+        if (!field) {
+            console.log(`El campo ${fieldName} no existe`);
+            continue;
+        }
+
+        // field.id_form_field
+        const formRegisterInsertedId = await insertFormFieldValue(formSchema.id_form, recordId, field.id_form_field, fieldValue);
+
+        ids.push(formRegisterInsertedId);
+    }
+
+    return {
+        recordId,
+        registerIds: ids
+    };
+}
+
+router.post("/api/apartarrayos/form/:name/submit", async (request, response) => {
+    console.log(request.fields);
+
+    const { name } = request.params;
+
+    try {
+        const formSchema = await getFormSchema(name);
+
+        const result = await insertFormRecord(formSchema, request.fields);
+
+        response.send({
+            success: true,
+            result
+        });
+    } catch (error) {
+        response.send({
+            success: false,
+            error: `Error al consultar los campos del formulario [${name}]`,
+            originalError: `${error}`
+        });
+    }
+})
 
 module.exports = router;
